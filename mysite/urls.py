6ed34cad711ac169839853ff3e8b3ff080e699da
d9magai <kumagai@sencorp.co.jp>
2019-12-06 15:09:34 +0000

    add the index view

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+from django.conf.urls import include
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
+    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
+
